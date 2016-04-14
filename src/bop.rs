a190a2a9a0c990488d328aa43a3615f6aebc65ea
zypeh <zypeh.geek@gmail.com>
2016-04-14 13:21:36 +0800

    Remove unused argument | Ready to publish

--- src/bop.rs
+++ src/bop.rs
@@ -1,18 +1,16 @@
 extern crate clap;
 use clap::{Arg, App};
 
+#[warn(unused_variables)]
 fn main() {
-    let matches = App::new("bop")
-                    .version("0.1")
-                    .author("zypeh <zypeh.geek@gmail.com>")
-                    .about("Experimental editor infrastructure")
-                    .arg(Arg::with_name("config")
-                               .short("c")
-                               .long("config")
-                               .value_name("TOML FILE")
-                               .help("Sets a custom config file")
-                               .takes_value(true))
-                    .get_matches();
-
-    //
+    App::new("bop")
+        .version("0.1")
+        .author("zypeh <zypeh.geek@gmail.com>")
+        .about("Experimental editor infrastructure")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("TOML FILE")
+            .help("Sets a custom config file")
+            .takes_value(true));
 }
