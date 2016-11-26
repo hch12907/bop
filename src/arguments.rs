5990be4dec00a4d6294f14bf898d02336c739222
zypeh <zypeh.geek@gmail.com>
2016-11-26 16:41:08 +0800

    shell completions script generation implementation

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,17 +1,25 @@
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 
 pub fn parse() -> App<'static, 'static> {
     App::new("Bop")
         // version
-        .version("0.1")
+        .version(crate_version!())
         // author information
-        .author("zypeh <zypeh.geek@gmail.com>")
+        .author(crate_authors!())
         // description
         .about("Experimental editor infrastructure")
 
         // test
         .arg(Arg::with_name("test")
-             .long("test")
-             .short("t")
-             .help("Prints the version number and license"))
+            .long("test")
+            .short("t")
+            .help("Prints the version number and license"))
+        
+        // generate the completion file for terminal
+        .subcommand(SubCommand::with_name("completions")
+            .about("Generates completion scripts for your shell")
+            .arg(Arg::with_name("SHELL")
+                .required(true)
+                .possible_values(&["bash", "fish", "zsh"])
+                .help("The shell to geenerate the script for")))
 }
--- src/bop.rs
+++ src/bop.rs
@@ -1,8 +1,23 @@
+#[macro_use]
 extern crate clap;
-use clap::{Arg, App};
+
+use std::io;
+use clap::Shell;
 
 mod arguments;
 
 fn main() {
     let matches = arguments::parse().get_matches();
+
+    match matches.subcommand() {
+        ("completions", Some(m)) => {
+            let sh = m.value_of("SHELL").unwrap();
+            arguments::parse()
+                .gen_completions_to(
+                    "bop",
+                    sh.parse::<Shell>().unwrap(),
+                    &mut io::stdout());
+        }
+        (_, _) => unimplemented!(),
+    }
 }
